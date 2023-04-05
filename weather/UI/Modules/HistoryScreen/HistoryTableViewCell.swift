1e0d0d7826c2bc6f7fb7426b41f42597d6120c69
Arina <karma.rin512@gmail.com>
2023-04-05 20:57:57 +0300

    add: history module

---
+++ weather/UI/Modules/HistoryScreen/HistoryTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  TableViewCell.swift
+//  weather
+//
+//  Created by Арина Моргачева on 05.04.2023.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    let cityNameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 20)
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(cityNameLabel)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupLayoutConstraints() {
+        let constraints = [
+            cityNameLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            cityNameLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
+}
---
+++ weather/UI/Modules/HistoryScreen/HistoryViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HistoryViewController.swift
+//  weather
+//
+//  Created by Арина Моргачева on 05.04.2023.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    let historyTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.allowsSelection = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.addSubview(historyTableView)
+        
+        setupLayoutConstraints()
+
+    }
+    
+    func setupLayoutConstraints() {
+        let conctraints = [
+            historyTableView.topAnchor.constraint(equalTo: view.topAnchor),
+            historyTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            historyTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            historyTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        ]
+        NSLayoutConstraint.activate(conctraints)
+    }
+  
+}
